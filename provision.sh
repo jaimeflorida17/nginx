cdae741a7a779c9890a4d72e19c05898d91e6d09
Jaime <jacagi@floridauniversitaria.es>
2020-10-27 13:06:41 +0100

    Copiado index a /vagrant/www, eliminado la ruta dicha, generando un enlace blando, destruyendo maquina e haciendo un "UP"

--- provision.sh
+++ provision.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
 sudo apt-get install nginx
+service ngnix status 
+cp -avr /usr/share/nginx/www /vagrant/www
+sudo rm -rf  /usr/share/nginx/www
+sudo ln -s /vagrant/www /usr/share/nginx/www
