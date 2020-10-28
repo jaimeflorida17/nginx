2b44f26877e0483f62a25e93f1036bb957139610
Jaime <jacagi@floridauniversitaria.es>
2020-10-28 11:28:35 +0100

    Solucionado problemas provision, copiando default, modificando default, eliminamos el enlace blando, destruyendo y levantando BOX

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure("2") do |config|
+
   config.vm.box = "hashicorp/precise32"
   config.vm.hostname = "web-dev"
   config.vm.provision "shell", path: "provision.sh"
   config.vm.network "forwarded_port", guest: 80, host: 8080, id: "nginx"
-  #config.vmsynced_folder "/etc/nginx/sites-enabled/default", "/var/www"
 end
--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
-sudo apt-get -y update
-sudo apt-get install -y nginx
-service ngnix status 
-cp -avr /usr/share/nginx/www /vagrant/www
-sudo rm -rf  /usr/share/nginx/www
-sudo ln -s /vagrant/www /usr/share/nginx/www
+apt-get -y update
+apt-get install -y nginx
+rm -rf /etc/nginx/sites-enabled
+rm -rf /usr/share/nginx/www
+cp -R /vagrant/sites-enabled  /etc/nginx/sites-enabled
+
+service nginx start
