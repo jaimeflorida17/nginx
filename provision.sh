05d688cb4933bfcf3aae1171924249b11de5de55
Jaime <jacagi@floridauniversitaria.es>
2020-10-28 11:53:31 +0100

    Deshabilitando carpeta default, sincronizando la carpeta www y sites enabled

--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 apt-get -y update
 apt-get install -y nginx
+cp -avr /usr/share/nginx/www/ /vagrant/www
 rm -rf /etc/nginx/sites-enabled
-rm -rf /usr/share/nginx/www
 cp -R /vagrant/sites-enabled  /etc/nginx/sites-enabled
+rm -rf /usr/share/nginx/www
 
 service nginx start
